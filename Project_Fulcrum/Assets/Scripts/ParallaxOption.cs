c279441f2b62485723aa2d5bf9e03b9821314695
Sean David McFarlane <sean.mcfarlane@hotmail.com>
2017-02-17 15:20:57 -0800

    Added parallax backgrounds!

---
+++ Project_Fulcrum/Assets/Scripts/ParallaxOption.cs
@@ -0,0 +1,24 @@
+﻿/*
+ ALL CREDIT FOR THIS CODE GOES TO DAVID DION-PAQUET AND HIS BLOG POST! Thanks Dave!
+ http://www.gamasutra.com/blogs/DavidDionPaquet/20140601/218766/Creating_a_parallax_system_in_Unity3D_is_harder_than_it_seems.php
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class ParallaxOption : MonoBehaviour {
+
+	public bool moveParallax;
+
+	[SerializeField]
+	[HideInInspector]
+	private Vector3 storedPosition;
+
+	public void SavePosition() {
+		storedPosition = transform.position;
+	}
+
+	public void RestorePosition() {
+		transform.position = storedPosition;
+	}
+}
